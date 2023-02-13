6b1755deab8bafeaa9c358ce6dade516b2a8dd96
Martini93829 <125286803+Martini93829@users.noreply.github.com>
2023-02-13 09:10:16 -0600

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,35 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let option1Button=document.querySelector(".option-one");
+
+let secondButton=document.querySelector(".optiontwo");
+
+let thirdButton=document.querySelector(".optionthree");
+
+let mexico=document.querySelector(".option-one");
+
+let brazil= document.querySelector(".option-two");
+
+let button1= document.querySelector(".option-one-screen");
+
+let button2=document.querySelector(".option-one-screen");
+
+
+
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
+
+
